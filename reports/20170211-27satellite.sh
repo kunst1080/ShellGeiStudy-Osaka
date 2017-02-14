4eac7121c25e27a000617d76fef785fba2b1a383
kunst1080 <kontrapunkt1080@gmail.com>
2017-02-14 23:45:48 +0900

    「第27回シェル芸勉強会　大阪サテライト」レポート

--- reports/20170211-27satellite.sh
+++ reports/20170211-27satellite.sh
@@ -1,69 +1,77 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
 output=$0.md
 
 ### Event Information ###
-nth=26
-date="12/25(日)"
+nth=27
+date="02/11(土)"
 title="「第${nth}回シェル芸勉強会　大阪サテライト」レポート"
 
-org_title="jus共催 第8回初心者の方角に向いて講師が喋るシェル勉強会（初心者向けとは言ってない）/第26回シェル芸勉強会及びエクシェル芸勉強会"
-org_url="https://usptomo.doorkeeper.jp/events/54573"
+org_title="jus共催 第9回初心者満足度ナンバーワン（当社調べ・調べてないけど）シェル勉強会/第27回sedこわいシェル芸勉強会"
+org_url="https://usptomo.doorkeeper.jp/events/56744"
 
-osaka_url="https://atnd.org/events/83966"
-fukuoka_url1="https://atnd.org/events/83997"
-fukuoka_url2="http://papiro.hatenablog.jp/entry/2016/12/26/011307"
+osaka_url="https://atnd.org/events/85597"
+fukuoka_url1="https://atnd.org/events/85463"
+fukuoka_url2="http://papiro.hatenablog.jp/entry/2017/02/12/162647"
 
-starting_slide="https://blog.ueda.asia/?presenpress=%E7%AC%AC26%E5%9B%9E%E3%82%B7%E3%82%A7%E3%83%AB%E8%8A%B8%E5%8B%89%E5%BC%B7%E4%BC%9A%E5%8F%8A%E3%81%B3%E3%82%A8%E3%82%AF%E3%82%B7%E3%82%A7%E3%83%AB%E8%8A%B8%E5%8B%89%E5%BC%B7%E4%BC%9A#/"
-question_url1="https://blog.ueda.asia/?p=9226"
-question_url2="https://blog.ueda.asia/?p=8833"
-togetter_url="https://togetter.com/li/1063223"
+starting_slide="https://blog.ueda.asia/?post_type=presenpress&p=9312#/"
+question_url1="https://blog.ueda.asia/?p=9309"
+question_url2="https://blog.ueda.asia/?p=9283"
+togetter_url="https://togetter.com/li/1080314"
+
+tokyo_LT="https://www.youtube.com/watch?v=JBax4En3EeM"
 
 ### Main Text ###
-bef_place_url1=""
+bef_place_url1="https://twitter.com/nmrmsys/status/830215577239117825"
 
-text_sankasya="すみません、年末年始忙しすぎ＆インフルエンザで更新がめっちゃ遅れました……"
+text_sankasya="今回の参加者は、私も含めて12人、うち、初参加は1名でした。\\n
+じわじわ初心者が増えてきてよいですね。\\n
+\\n
+また今回、西日本では大雪の心配があったので、前日にメールとTwitterで注意喚起の告知を行いました。
+[https://twitter.com/kunst1080/status/830016175849381888:embed]
+(@t_motookaさん、促して下さってありがとうございました！)"
 
 
 # 午前1
-am1_title=
-am1_txet="今回、午前の部は私用で欠席していたので、午前の内容については他の方のレポを見てくださいです。"
-am1_url=
-am1_url2=
+am1_title="鳥海秀一さん「黒い画面と戯れよう」"
+am1_txet="sttyコマンドやtputコマンドを使って、ターミナルにエスケープシーケンスを送って遊ぶという感じの内容でした。\\n
+ターミナル生活をしていく上で、この辺のことを知っていると思わぬところで救われそうな感じですね。"
+am1_url="https://www.youtube.com/watch?v=oeNFq5t_frc"
+am1_url2="[https://twitter.com/t_motooka/status/830228032795217920:embed]"
 
 # 午前2
-am2_title=
-am2_txet=
-am2_url=
-am2_url2=
+am2_title="石井久治さん(@hisaharu)「シェル芸入門 日常会話編」"
+am2_txet="シェル・コマンド・パイプなどについて、図解でわかりやすく解説した講義でした。初心者の方は必見だと思います！"
+am2_url="https://www.youtube.com/watch?v=cDt2Olr0x84"
+am2_url2="[https://twitter.com/obknnt/status/830238613497475073:embed]"
 
 # 昼休み
-lunch_text="昼休みから参加しました。大阪サテライトでは、@t_motookaさんによって、「CVE番号クイズ」「バイナリを詠む会」「エクシェル芸の予習」などがゲリラ実行されました！\\n
-特に予習は、午後の部の問題を特に当たって非常に役立ちました。ありがとうございます！"
+lunch_text="大阪はお昼ごはんを食べつつ、午後に向けてsedの予習をしておりました。嵐の前の静けさとはこのこと――"
 
-lunch_url1="https://twitter.com/kunst1080/status/812859526936301568"
-lunch_url2="https://twitter.com/nmrmsys/status/812861445708709888"
-lunch_url3="https://twitter.com/kunst1080/status/812862135659134976"
-lunch_url4="https://twitter.com/t_motooka/status/812867240877924352"
-lunch_url5="https://twitter.com/kunst1080/status/812870294364749824"
-lunch_url6="https://twitter.com/t_motooka/status/812871680796200960"
-
-
+lunch_url1="https://twitter.com/kunst1080/status/830251969235148800"
+lunch_url2=
+lunch_url3=
+lunch_url4=
+lunch_url5=
+lunch_url6=
 
 # 午後
-text_pm="エクシェル芸（泡を吹いて死亡"
-text_pm_url1="https://twitter.com/t_motooka/status/812872021637885953"
-text_pm_url2="https://twitter.com/ryuichiueda/status/812918105705877504"
+pm_text1="sedこわい（こわい"
+pm_url1="https://twitter.com/kunst1080/status/830267156793208834"
+pm_url2="https://twitter.com/kunst1080/status/830282631652663296"
+pm_url3="https://twitter.com/kunst1080/status/830305632615936000"
+pm_text2="sedで置換以外のことをする方法や、コマンドを並べて各方法、ループの使い方など、置換以外の基本的な機能を使い倒した感じです。\\n
+ループはあとでもう一回復習しよう……"
 
 # 追記的な
-epilogue_url=
+epilogue="危険な派生物が誕生してしまったようです…\\n
+[https://togetter.com/li/1080646:embed]"
 
 
 ### LT ###
-LT_summary="いつもな感じのやばい大阪勢のLTです。今回はUstreamで配信もして頂けました！(録画はナシです)\\n
-[https://twitter.com/kunst1080/status/812919757800292353:embed]"
+LT_summary="今回はちょっと少なめですが、今回もUstreamで配信して頂けました！(録画はナシです)"
 
 # LT0_user=""
 # LT0_title=""
@@ -71,48 +79,37 @@ LT_summary="いつもな感じのやばい大阪勢のLTです。今回はUstrea
 # LT0_explain=""
 # LT0_twitter_url1=""
 
-LT1_user="nmrmsys(@nmrmsys) さん"
-LT1_title="Excelでもコマンド実行がしたい！"
-LT1_slide_url="http://qiita.com/nmrmsys/items/0c814efcf4461e45f758"
-LT1_explain="いつもの「ExSQell」のお話ですね。今回は新機能として「セル範囲リダイレクト」が追加されたようです。リポジトリはこちら → [https://github.com/mikkun/gnucash-util-jp:title]"
-LT1_twitter_url1="https://twitter.com/t_motooka/status/812933386322919424"
+LT1_user="日柳 光久(@mikkun_jp) さん"
+LT1_title="Q7の解説"
+LT1_slide_url="https://twitter.com/mikkun_jp/status/830297680937000960"
+LT1_explain="Q7で「縛り1: 使うコマンドはseq、cp、sedだけ」と「縛り2: ワンライナー中で数字を使わない」という縛りがあったのですが、
+その両方の条件を満たす解とその解説です。\\n
+カジュアルに\`/etc/passwd\`を使うことに若干の危険なかほりを感じますｗ"
+LT1_twitter_url1="https://twitter.com/mikkun_jp/status/830301542225829888"
 
-LT2_user="くんすと(@kunst1080)"
-LT2_title="Hyper™でエキサイティングなシェル芸ライフ"
-LT2_slide_url="http://www.slideshare.net/kunst1080/hyper-70428553"
-LT2_explain="Hyperというターミナルエミュレータを使って楽しくシェル芸しましょう、という話です。Hyperの公式サイトはこちら → [https://hyper.is/:title]"
-LT2_twitter_url1="https://twitter.com/3socha/status/812934789426593792"
-LT2_twitter_url2="https://twitter.com/nmrmsys/status/812935122752184321"
+LT2_user="@t_motooka さん"
+LT2_title="sedで知る 矢印キーのキーコード"
+LT2_slide_url="https://speakerdeck.com/motooka/seddezhi-ru-shi-yin-kifalse-kikodo"
+LT2_explain="\`sed -n l\`してからキー入力し、Enterを押すと、ターミナル上に矢印キーのキーコードが表示されます。やってみましょう！"
+LT2_twitter_url1="https://twitter.com/kunst1080/status/830324962300813312"
+LT2_twitter_url2="https://twitter.com/kunst1080/status/830325926831411200"
+LT2_twitter_url3="https://twitter.com/msr386/status/830326083069186049"
 
-LT3_user="so(@3socha) さん"
-LT3_title="FUSE で繋がる世界"
-LT3_slide_url="https://horo17.github.io/calfs-slide/"
-LT3_explain="FUSEを使ってGoogleカレンダーのファイルシステムを実装したという話です。予定を探すときにfindとかgrepとかが使えて便利そう！"
-LT3_twitter_url1="https://twitter.com/nmrmsys/status/812936781955932160"
-LT3_twitter_url2="https://twitter.com/nmrmsys/status/812938765501014016"
-LT3_twitter_url3="https://twitter.com/kunst1080/status/812939056237527040"
-LT3_twitter_url4="https://twitter.com/t_motooka/status/812939217051394049"
-LT3_twitter_url5="https://twitter.com/nmrmsys/status/812939316997390336"
+LT3_user="@nmrmsys さん"
+LT3_title="我は放つ、危険のシェル芸！！(誤爆)"
+LT3_slide_url="https://dl.dropboxusercontent.com/u/54939588/slides/I-Will-Release-Dangerous-Shell-Arts.html"
+LT3_explain="「[スター・ウォーズ Force Band™ by Sphero](https://www.amazon.co.jp/%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%BB%E3%82%A6%E3%82%A9%E3%83%BC%E3%82%BA-Force-Sphero-%E3%80%90%E6%97%A5%E6%9C%AC%E6%AD%A3%E8%A6%8F%E4%BB%A3%E7%90%86%E5%BA%97%E5%93%81%E3%80%91-AFB01JPN/dp/B01L8GHXSQ)」というガジェットを使って、スターウォーズのジェスチャーで危険シェル芸を放つ！という実演でした。普通の危険シェル芸だと一気にサーバがダウンしてしまうのですが、今回は徐々にダウンしていく状況を確認するべく「マイルドな危険シェル芸」を仕込んで来ていただきました！\\n
+\\n
+今回はサーバ側のトラブルで不発でした……ので、できれば次回にリベンジしていただければと！　よろしくお願いします！"
+LT3_twitter_url1="https://twitter.com/kunst1080/status/830329641902563330"
+LT3_twitter_url2="https://twitter.com/kunst1080/status/830331105463062528"
+LT3_twitter_url3="https://twitter.com/kunst1080/status/830342697428750336"
+LT3_twitter_url4="https://twitter.com/kunst1080/status/830343561644158979"
+LT3_twitter_url5="https://twitter.com/kunst1080/status/830344331856793602"
 
-LT4_user="MSR(@msr386) さん"
-LT4_title="漢字バナー芸"
-LT4_slide_url="https://speakerdeck.com/msr_i386/kanji-banner"
-LT4_explain="toliet(1)で、漢字を表示するために漢字フォントを作るという話です。実際の手順の説明と、デモを見せて頂きました。"
-LT4_twitter_url1="https://twitter.com/kunst1080/status/812942898471661568"
-LT4_twitter_url2="https://twitter.com/3socha/status/812942930059005952"
-LT4_twitter_url3="https://twitter.com/mikkun_jp/status/812944071614283776"
-
-LT5_user="社畜(9か月)(@null_ref_eng) さん ※延長線"
-LT5_title="画面を\(^o^)/で埋め尽くす謎のワンライナー"
-LT5_slide_url="https://twitter.com/null_ref_eng/status/812983458469249024"
-LT5_explain="滅茶苦茶面白いことをされていたので、半ば強引に発表していただきましたｗ　無理言ってすみませんでした…"
-LT5_twitter_url1="https://twitter.com/t_motooka/status/812945261576089600"
-LT5_twitter_url2="https://twitter.com/kunst1080/status/812945742574723072"
-LT5_twitter_url3="https://twitter.com/nmrmsys/status/812946099363119104"
-LT5_twitter_url3="https://twitter.com/kunst1080/status/812947076166209541"
 
 # まとめ
-LT_maeome="今回も面白いLTをたくさん発表していただいてありがとうございました！！　次回もモリモリ発表してくださいねっ！"
+LT_maeome=
 
 matome="上田会長、東京・福岡・大阪の皆様、お疲れ様でした。\\n
 フェンリル様、会場使わせて下さってありがとうございました！\\n
@@ -171,7 +168,7 @@ $([ "$bef_place_url1" != "" ] && echo "[${bef_place_url1}:embed]")
 
 ## 福岡サテライト
 [${fukuoka_url1}:embed:cite]
-$([ "$fukuoka_url2" != "" ] && echo "[https://twitter.com/papiron:title=ぱぴろん(@papiron)]さんによるまとめ")
+$([ "$fukuoka_url2" != "" ] && echo "### [https://twitter.com/papiron:title=ぱぴろん(@papiron)]さんによるまとめ")
 $([ "$fukuoka_url2" != "" ] && echo "[${fukuoka_url2}:embed:cite]")
 
 
@@ -204,14 +201,19 @@ $([ "$lunch_url6" != "" ] && echo "[${lunch_url6}:embed]\\n")
 
 
 ### 午後
-${text_pm}
-$([ "$text_pm_url1" != "" ] && echo "[${text_pm_url1}:embed]")
-$([ "$text_pm_url2" != "" ] && echo "[${text_pm_url2}:embed]")
+${pm_text1}
+$([ "$pm_url1" != "" ] && echo "[${pm_url1}:embed]")
+$([ "$pm_url2" != "" ] && echo "[${pm_url2}:embed]")
+$([ "$pm_url3" != "" ] && echo "[${pm_url3}:embed]")
 
-※詳細はtogetterの方を参照です。
+$pm_text2
 
 
-## LTのようす
+# LTのようす
+$([ "$tokyo_LT" != "" ] && echo "## 東京")
+$([ "$tokyo_LT" != "" ] && echo "[${tokyo_LT}:embed]")
+
+## 大阪
 ${LT_summary}
 
 $(writeLT)
@@ -221,7 +223,7 @@ $LT_maeome
 # まとめ
 $matome
 
-$([ "$epilogue_url" != "" ] && echo "### 追記")
-$([ "$epilogue_url" != "" ] && echo "[${epilogue_url}:embed]")
+$([ "$epilogue" != "" ] && echo "### 追記")
+$([ "$epilogue" != "" ] && echo "${epilogue}")
 
 ++EOS
